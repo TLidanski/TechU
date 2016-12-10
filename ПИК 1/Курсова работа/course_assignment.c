12433d710ee09069e3effc4b265fbf901968a8ad
T Lidanski <t_lidanski@abv.bg>
2016-12-10 21:46:40 +0200

    Pucai Kume

--- ПИК 1/Курсова работа/course_assignment.c
+++ ПИК 1/Курсова работа/course_assignment.c
@@ -6,6 +6,7 @@ void readFileWriteFile();
 void readFileWriteScreen();
 void readKeyboardWriteFile();
 void readKeyboardWriteScreen();
+char* getFileExtension(char fileName[]);
 
 int main() {
 	printf("Select:\n1 - Read from file, write to file\n2 - Read from file, write to screen\n3 - Read from keyboard, write to file\n4 - Read from keyboard, write to screen\n");
@@ -37,25 +38,62 @@ int main() {
 
 void readFileWriteFile() {
 	FILE *fp;
-	char fileToRead[20];  //Name of file we'll read from
-	char fileToWrite[20];  //Name of file where we'll write
+	char fileToRead[20];  // Name of file we'll read from
+	char fileToWrite[20];  // Name of file where we'll write
+
+	char buffer[250];  // Contains the lines read by fgets
+	char longestLine[250];
+	int longestLength = 0;
 
 	printf("Enter the name of the file you want to read\n");
 	scanf("%s", fileToRead);
+
+	if(strcmp(getFileExtension(fileToRead), "c") != 0) {
+		printf("Invalid extension");
+		exit(0);
+	}
+
 	printf("Enter the name of the file where you want to write\n");
 	scanf("%s", fileToWrite);
 
 	fp = fopen(fileToRead, "r");
 	if(fp == NULL) {
 		perror("Error: ");
-		return 1;
+		return 1;  // Might need to be exits due to void type
 	}
+
+	while(fgets(buffer, sizeof buffer, fp) != NULL) {
+		if(strlen(buffer) > longestLength) {  // If the line contains a printf with a \n character the logic screws up or not
+			longestLength = strlen(buffer);
+			strcpy(longestLine, buffer);
+		}
+	}
+
+	fclose(fp);
+
+	fp = fopen(fileToWrite, "w");
+	if(fp == NULL) {
+		perror("Error: ");
+		return 1;  // Might need to be exits due to void type
+	}
+
+	fprintf(fp, "Longest line:\n %s\nIdentifier char count:\n %d\n", longestLine, 24);
+
+	fclose(fp);
 }
 
-void readFileWriteScreen() {
-
-}
+void readFileWriteScreen() {}
 
 void readKeyboardWriteFile() {}
 
 void readKeyboardWriteScreen() {}
+
+char* getFileExtension(char fileName[]) {
+	char *pch;
+
+	pch = strtok(fileName, ".");
+	if(pch != NULL)
+		pch = strtok(NULL, ".");
+
+	return pch;
+}
