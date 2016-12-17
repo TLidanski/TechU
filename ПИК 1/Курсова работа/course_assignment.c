c78365ab4f9d8dbd66644eab9f555f79ac96eed0
T Lidanski <t_lidanski@abv.bg>
2016-12-17 16:11:51 +0200

    Committing the first two functions of the assignment

--- ПИК 1/Курсова работа/course_assignment.c
+++ ПИК 1/Курсова работа/course_assignment.c
@@ -6,7 +6,10 @@ void readFileWriteFile();
 void readFileWriteScreen();
 void readKeyboardWriteFile();
 void readKeyboardWriteScreen();
+char* getLongestLine(FILE *fp);
+int getIdentifierCharCount(FILE *fp);
 char* getFileExtension(char fileName[]);
+int isDataType(char *val);
 
 int main() {
 	printf("Select:\n1 - Read from file, write to file\n2 - Read from file, write to screen\n3 - Read from keyboard, write to file\n4 - Read from keyboard, write to screen\n");
@@ -41,9 +44,8 @@ void readFileWriteFile() {
 	char fileToRead[20];  // Name of file we'll read from
 	char fileToWrite[20];  // Name of file where we'll write
 
-	char buffer[250];  // Contains the lines read by fgets
 	char longestLine[250];
-	int longestLength = 0;
+	int idCharCount;
 
 	printf("Enter the name of the file you want to read\n");
 	scanf("%s", fileToRead);
@@ -62,12 +64,9 @@ void readFileWriteFile() {
 		exit(0);
 	}
 
-	while(fgets(buffer, sizeof buffer, fp) != NULL) {
-		if(strlen(buffer) > longestLength) {  // If the line contains a printf with a \n character the logic screws up or not
-			longestLength = strlen(buffer);
-			strcpy(longestLine, buffer);
-		}
-	}
+	strcpy(longestLine, getLongestLine(fp));
+	rewind(fp);
+	idCharCount = getIdentifierCharCount(fp);
 
 	fclose(fp);
 
@@ -77,19 +76,89 @@ void readFileWriteFile() {
 		exit(0);
 	}
 
-	fprintf(fp, "Longest line:\n %s\nIdentifier char count:\n %d\n", longestLine, 24);
+	fprintf(fp, "Longest line:\n %s\nIdentifier char count:\n %d\n", longestLine, idCharCount);
 
 	fclose(fp);
 }
 
-void readFileWriteScreen() {}
+void readFileWriteScreen() {
+	FILE *fp;
+	char fileToRead[20];  // Name of file we'll read from
+
+	char longestLine[250];
+	int idCharCount;
+
+	printf("Enter the name of the file you want to read\n");
+	scanf("%s", fileToRead);
+
+	if(strcmp(getFileExtension(fileToRead), "c") != 0) {
+		printf("Invalid extension");
+		exit(0);
+	}
+
+	fp = fopen(fileToRead, "r");
+	if(fp == NULL) {
+		perror("Error: ");
+		exit(0);
+	}
+
+	strcpy(longestLine, getLongestLine(fp));
+	rewind(fp);
+	idCharCount = getIdentifierCharCount(fp);
+
+	fclose(fp);
+
+	printf("Longest line:\n %s\nIdentifier char count:\n %d\n", longestLine, idCharCount);
+}
 
 void readKeyboardWriteFile() {}
 
 void readKeyboardWriteScreen() {}
 
+char* getLongestLine(FILE *fp) {
+	char buffer[250];  // Contains the lines read by fgets
+	char *longestLine;
+	int longestLength = 0;
+
+	while(fgets(buffer, sizeof buffer, fp) != NULL) {  // If the line contains a printf with a \n character the logic might screw up
+		if(strlen(buffer) > longestLength) {  
+			longestLength = strlen(buffer);
+			strcpy(longestLine, buffer);
+		}
+	}
+
+	return longestLine;
+}
+
+int getIdentifierCharCount(FILE *fp) {  
+	char buffer[30];  // Contains the strings read by fscanf
+	int identifierCharCount = 0;
+	int isIdentifier = 0;
+	int i;
+
+	while(fscanf(fp, "%s", buffer) == 1) {
+		if(isIdentifier) {
+			for (i = 0; i < strlen(buffer); ++i) {
+
+				if(buffer[i] >= 'a' && buffer[i] <= 'z')
+					identifierCharCount++; 
+					// printf("%c\n", buffer[i]);
+
+				if(buffer[i] == '(')
+					break;
+			}
+		} 	
+		isIdentifier = 0;
+
+		if(isDataType(buffer))
+			isIdentifier = 1;
+	}
+
+	return identifierCharCount;
+}
+
 char* getFileExtension(char fileName[]) {
-	char *pch;
+	char *pch;  // Char pointer, cointains the parts of the split string
 	char str[20];
 
 	strcpy(str, fileName);
@@ -97,4 +166,16 @@ char* getFileExtension(char fileName[]) {
 	pch = strtok(NULL, ".");
 
 	return pch;
+}
+
+int isDataType(char *val) {
+	int i;
+	char *dataTypes[] = {"int", "double", "float", "char", "void"};
+	int arrSize = sizeof dataTypes/sizeof dataTypes[0];
+
+	for (i = 0; i < arrSize; ++i) {
+		if(strcmp(dataTypes[i], val) == 0)
+			return 1;
+	}
+	return 0;
 }
