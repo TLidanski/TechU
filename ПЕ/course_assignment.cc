9be1698d42f17655b8a2c92a2aac89474335b275
T Lidanski <t_lidanski@abv.bg>
2018-11-10 12:50:28 +0200

    Adding the library dependancy as well as other functionalities

--- ПЕ/course_assignment.cc
+++ ПЕ/course_assignment.cc
@@ -6,12 +6,27 @@
 #include <iterator>
 #include <memory>
 #include <initializer_list>
-#include <functional> // reference wrapper
-#include<stdarg.h>
+#include <functional>
 
-#define MANUFACTURER_FILE "manufacturers.txt"
+#include "cereal/cereal.hpp"
+#include "cereal/access.hpp"
+#include "cereal/macros.hpp"
+
+#include "cereal/archives/json.hpp"
+#include "cereal/types/vector.hpp"
+#include "cereal/types/list.hpp"
+
+#define MANUFACTURER_FILE "manufacturers.json"
+#define OPTICS_FILE "optics.json"
  
 using namespace std;
+using namespace cereal;
+
+class Optic;
+class OpticsManufacturer;
+
+vector<OpticsManufacturer> globalManufacturers;
+vector<Optic> globalOptics;
 
 class Optic {
 private:
@@ -19,12 +34,17 @@ private:
 	int width;
 	int size;
 	string material;
+	double price;
+
 public:
-	Optic(string type, int width, int size, string material) {
+	Optic() {}
+
+	Optic(string type, int width, int size, string material, double price) {
 		this->type = type;
 		this->width = width;
 		this->size = size;
 		this->material = material;
+		this->price = price;
 	}
 
 	Optic(const Optic &optic) {
@@ -32,6 +52,12 @@ public:
 		width = optic.width;
 		size = optic.size;
 		material = optic.material;
+		price = optic.price;
+	}
+
+	template<class Archive>
+	void serialize(Archive & archive) {
+    	archive(type, width, size, material, price); 
 	}
 
 	string getType() {
@@ -49,6 +75,10 @@ public:
 	string getMaterial() {
 		return this->material;
 	}
+
+	double getPrice() {
+		return this->price;
+	}
 };
 
 class OpticsManufacturer {
@@ -60,11 +90,18 @@ private:
 	list<Optic> availableOptics;
 
 public:
+	OpticsManufacturer() {}
+
 	OpticsManufacturer(string bulstat, string name, string address, string phoneNumber) {
 		this->bulstat = bulstat;
 		this->name = name;
 		this->address = address;
 		this->phoneNumber = phoneNumber;
+	}
+
+	template<class Archive>
+	void serialize(Archive & archive) {
+    	archive(bulstat, name, address, phoneNumber, availableOptics); 
 	}
 
 	string getBulstat() {
@@ -87,36 +124,63 @@ public:
 		availableOptics.push_back(optic);
 	}
 
-	void printAvailableOptics() {
-		for (list<Optic>::iterator i = availableOptics.begin(); i != availableOptics.end(); ++i) {
-			//cout << "Company Bulstat: " << i->bulstat << endl << "Company Name: " << i->name << endl << "Address" << i->address << endl << "Phone Number" << i->phoneNumber << endl;
-			cout << "Optics Type: " << i->getType() << endl << "Width: " << i->getWidth() << endl << "Size: " << i->getSize() << endl << "Optics Material: " << i->getMaterial() << endl << endl;
+	friend ostream& operator << (ostream& out, OpticsManufacturer &man) {
+		out << "Company Bulstat: " << man.bulstat << endl << "Company Name: " << man.name << endl << "Address" << man.address << endl << "Phone Number" << man.phoneNumber << endl;
+
+		for (list<Optic>::iterator i = man.availableOptics.begin(); i != man.availableOptics.end(); ++i) {
+			
+			out << "Optics Type: " << i->getType() << endl << "Width: " << i->getWidth() << endl << "Size: " << i->getSize() << endl << "Optics Material: " << i->getMaterial() << endl << "Price: " << i->getPrice() << endl << endl;
 		}
+
+		return out;
 	}
+
 };
 
 class IOUtils {
 private:
-	string manufacturersFile;
+	
 
 public:
-	IOUtils() {
-		manufacturersFile = MANUFACTURER_FILE;
-	}
+	IOUtils() {}
 
-	void writeManufacturerToFile(OpticsManufacturer manufacturer) {
+	template <typename T, typename A>
+	void readFile(string fileName, vector<T, A> &collection) {
 		fstream file;
-		file.open(manufacturersFile.c_str(), ios::app);
+		file.open(fileName.c_str(), ios::in);
 
 		if(file.is_open()) {
-			// file.write((char*)&manufacturer, sizeof(OpticsManufacturer));
+			{
+				JSONInputArchive archiveIn(file);
+				archiveIn(collection);
+			}
 
-			// file.close();
-
+			file.close();
 		} else {
 
 			cout << "File could not be opened" << endl;
 		}
+
+	}
+
+	template <typename T, typename A>
+	void writeToFile(string fileName, vector<T, A> &collection) {
+		fstream file;
+		file.open(fileName.c_str(), ios::out | ios::trunc);
+
+		if(file.is_open()) {
+			{
+				JSONOutputArchive archiveOut(file);
+				archiveOut(CEREAL_NVP(collection));
+			}
+
+
+			file.close();
+		} else {
+
+			cout << "File could not be opened" << endl;
+		}
+		
 	}
 };
 
@@ -143,9 +207,25 @@ public:
 	RegisterManufacturerItem(const char* title): MenuItem(title) {}
 
 	void execute() {
-		cout << "Executed Register Item" << endl;
+		string bulstat, name, address, phone;
 
-		
+		cin.ignore(1, '\n');
+
+		cout << "Enter the company BULSTAT" << endl;
+		getline(cin, bulstat);
+
+		cout << "Enter the company's name" << endl;
+		getline(cin, name);
+
+		cout << "Enter an address" << endl;
+		getline(cin, address);
+
+		cout << "Enter a phone number" << endl;
+		getline(cin, phone);
+
+		OpticsManufacturer manufacturer(bulstat, name, address, phone);		
+
+		globalManufacturers.push_back(manufacturer);
 	}
 };
 
@@ -153,7 +233,44 @@ class RegisterOpticItem : public MenuItem {
 public:
 	RegisterOpticItem(const char* title): MenuItem(title) {}
 
-	void execute() {}
+	void execute() {
+		double price;
+		int width, size;
+		string type, material;
+
+		cin.ignore(1, '\n');
+
+		cout << "Enter the optic size" << endl;
+		getline(cin, type);
+
+		cout << "Enter the width" << endl;
+		cin >> width;
+
+		cout << "Enter the size" << endl;
+		cin >> size;
+		cin.ignore(1, '\n');
+
+		cout << "Enter the material" << endl;
+		getline(cin, material);
+
+		cout << "Enter the price" << endl;
+		cin >> price;
+
+		Optic optic(type, width, size, material, price);
+
+		globalOptics.push_back(optic);
+	}
+};
+
+class ExitItem : public MenuItem {
+public:
+	ExitItem(const char* title): MenuItem(title) {}
+
+	void execute() {
+		IOUtils io;
+
+		io.writeToFile(MANUFACTURER_FILE, globalManufacturers);
+	}
 };
 
 template <class T>
@@ -175,7 +292,7 @@ public:
 
 		cout << "Select:" << endl;
 		for(int i = 0; i < items.size(); i++) {
-			
+
 			cout << i+1 << ". " << items[i].get().getTitle() << endl;
 		}
 	}
@@ -196,10 +313,20 @@ public:
 };
 
 int main() {
+	IOUtils io;
+
 	RegisterManufacturerItem registerManufacturer("Register Manufacturer");
 	RegisterOpticItem registerOptic("Register Optic");
+
+	ExitItem exit("Exit");
 	
-	Menu<MenuItem> menu({registerManufacturer, registerOptic});
+	Menu<MenuItem> menu({
+		registerManufacturer, 
+		registerOptic,
+		exit
+	});
+
+	io.readFile(MANUFACTURER_FILE, globalManufacturers);
 
 	menu.start();
 
